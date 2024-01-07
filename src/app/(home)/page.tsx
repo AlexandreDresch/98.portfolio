92de355c5ac72606a2db68f0971da6d0ed35c538
AlexandreDresch <z3r0PC@protonmail.com>
2024-01-07 00:23:42 -0300

    feat: home page moved to folder

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import Dock from "@/components/ui/dock";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#0c7f80]">
+     <Dock />
+    </main>
+  )
+}
