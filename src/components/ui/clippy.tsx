975b79a232f69912b614c51a09ca650ff6e0488b
AlexandreDresch <z3r0PC@protonmail.com>
2024-01-16 00:26:06 -0300

    feat: add clippy component

---
+++ src/components/ui/clippy.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./dropdown-menu";
+
+export default function Clippy() {
+  return (
+    <div className="absolute bottom-14 right-14">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Image
+            width={0}
+            height={0}
+            alt="folder"
+            src={"/clippy.gif"}
+            className="w-auto h-14 cursor-pointer"
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="top">
+          Can I help you?
+          <DropdownMenuSeparator />
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
