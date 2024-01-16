db111f9e0e63a5308475a90a93d3018fb82ddfce
AlexandreDresch <z3r0PC@protonmail.com>
2024-01-16 00:32:10 -0300

    feat: add shortcuts to desktop

--- src/components/ui/desktop.tsx
+++ src/components/ui/desktop.tsx
@@ -1,7 +1,18 @@
-export default function Desktop() {
-    return (
-        <div className="min-w-[100dvw] min-h-[100dvh] relative overflow-hidden">
+import Clippy from "./clippy";
+import Shortcut from "./shortcut";
 
-        </div>
-    )
+export default function Desktop() {
+  return (
+    <div className="pt-4 pl-4 grid grid-flow-col grid-cols-10 grid-rows-8 min-w-[100dvw] min-h-[100dvh] relative overflow-hidden">
+        <Shortcut />
+        <Shortcut />
+        <Shortcut />
+        <Shortcut />
+        <Shortcut />
+        <Shortcut />
+        <Shortcut />
+        <Clippy />
+    </div>
+    
+  );
 }
