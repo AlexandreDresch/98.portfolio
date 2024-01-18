936927779ac4e2fb9b137d92df8fc9ffdf3cbdae
AlexandreDresch <z3r0PC@protonmail.com>
2024-01-18 00:02:37 -0300

    feat: add props to shortcut components

--- src/components/ui/desktop.tsx
+++ src/components/ui/desktop.tsx
@@ -3,14 +3,15 @@ import Shortcut from "./shortcut";
 
 export default function Desktop() {
   return (
-    <div className="pt-4 pl-4 grid grid-flow-col grid-cols-10 grid-rows-8 min-w-[100dvw] min-h-[100dvh] relative overflow-hidden">
-        <Shortcut />
-        <Shortcut />
-        <Shortcut />
-        <Shortcut />
-        <Shortcut />
-        <Shortcut />
-        <Shortcut />
+    <div className="pt-4 grid grid-flow-col grid-cols-12 grid-rows-8 min-w-[100dvw] min-h-[100dvh] relative overflow-hidden">
+        <Shortcut image="/folder.png" name="BackEnd"/>
+        <Shortcut image="/folder.png" name="FrontEnd"/>
+        <Shortcut image="/folder.png" name="Mobile"/>
+        <Shortcut image="/folder.png" name="BackEnd"/>
+        <Shortcut image="/folder.png" name="BackEnd"/>
+        <Shortcut image="/folder.png" name="BackEnd"/>
+        <Shortcut image="/folder.png" name="BackEnd"/>
+        <Shortcut image="/folder.png" name="BackEnd"/>
         <Clippy />
     </div>
     
