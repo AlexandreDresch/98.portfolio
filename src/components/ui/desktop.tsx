88bf4fe3cba6c64e37b30346e91a2bcb2697b3f9
AlexandreDresch <z3r0PC@protonmail.com>
2024-01-18 23:10:48 -0300

    style: grid changed to display flex

--- src/components/ui/desktop.tsx
+++ src/components/ui/desktop.tsx
@@ -3,7 +3,7 @@ import Shortcut from "./shortcut";
 
 export default function Desktop() {
   return (
-    <div className="pt-4 grid grid-flow-col grid-cols-12 grid-rows-8 min-w-[100dvw] min-h-[100dvh] relative overflow-hidden">
+    <div className="pt-4 flex flex-col gap-8 min-w-[100dvw] min-h-[100dvh] relative overflow-hidden">
         <Shortcut image="/folder.png" name="BackEnd"/>
         <Shortcut image="/folder.png" name="FrontEnd"/>
         <Shortcut image="/folder.png" name="Mobile"/>
