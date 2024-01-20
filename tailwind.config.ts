91092273b6adbbc1070eb44d844fd034e2718e9a
AlexandreDresch <z3r0PC@protonmail.com>
2024-01-20 00:22:00 -0300

    style: add rainbow gradient

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      backgroundImage: ({ theme }) => ({
+        rainbow: `linear-gradient(to right, ${theme('colors.red.500')},${theme('colors.yellow.500')},${theme('colors.green.500')},${theme('colors.blue.500')})`,
+      }),
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
