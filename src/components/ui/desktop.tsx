e5e8352d5bf581af06e03ab90219f9d3e76e20b6
AlexandreDresch <z3r0PC@protonmail.com>
2024-01-23 23:50:53 -0300

    feat: update desktop shortcuts props

--- src/components/ui/desktop.tsx
+++ src/components/ui/desktop.tsx
@@ -4,14 +4,32 @@ import Shortcut from "./shortcut";
 export default function Desktop() {
   return (
     <div className="pt-4 flex flex-col gap-8 min-w-[100dvw] min-h-[100dvh] relative overflow-hidden">
-      <Shortcut image="/recycle-bin.png" name="Recycle Bin" />
-      <Shortcut image="/computer-explorer.png" name="My Computer" />
-      <Shortcut image="/folder.png" name="BackEnd" />
-      <Shortcut image="/folder.png" name="FrontEnd" />
-      <Shortcut image="/folder.png" name="Mobile" />
-      <Shortcut image="/file.png" name="My Resume" />      
-      <Shortcut image="/help-book.png" name="About the Project" />
-      <Shortcut image="/modem.png" name="Contact me" />
+      <Shortcut
+        image="/recycle-bin.png"
+        name="Recycle Bin"
+        isDocument={false}
+      />
+      <Shortcut
+        image="/computer-explorer.png"
+        name="My Computer"
+        isDocument={false}
+      />
+      <Shortcut image="/folder.png" name="BackEnd" isDocument={false} />
+      <Shortcut image="/folder.png" name="FrontEnd" isDocument={false} />
+      <Shortcut image="/folder.png" name="Mobile" isDocument={false} />
+      <Shortcut
+        image="/file.png"
+        name="My Resume"
+        isDocument
+        documentPath="./englishCV.pdf"
+      />
+      <Shortcut
+        image="/help-book.png"
+        name="About the Project"
+        isDocument
+        documentPath=""
+      />
+      <Shortcut image="/modem.png" name="Contact me" isDocument={false} />
       <Clippy />
     </div>
   );
