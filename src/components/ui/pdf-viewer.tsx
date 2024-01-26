9a90392180b2bd670754f442bc21f9e924dfa1bc
AlexandreDresch <z3r0PC@protonmail.com>
2024-01-25 23:50:38 -0300

    feat: add loading to pdf viewer

--- src/components/ui/pdf-viewer.tsx
+++ src/components/ui/pdf-viewer.tsx
@@ -3,8 +3,9 @@
 import { useState } from "react";
 import { Document, Page } from "react-pdf";
 import { pdfjs } from "react-pdf";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "./separator";
+import HourglassLoading from "./hourglass-loading";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
@@ -20,7 +21,14 @@ export default function PdfViewer({ documentPath }: { documentPath: string }) {
 
   return (
     <ScrollArea className="w-full h-full bg-white">
-      <Document file={documentPath} onLoadSuccess={onDocumentLoadSuccess}>
+      <Document
+        file={documentPath}
+        onLoadSuccess={onDocumentLoadSuccess}
+        loading={
+          <HourglassLoading className="w-full h-full flex mt-40 justify-center items-center" />
+        }
+        noData={"Page not found."}
+      >
         {(Array.apply(null, Array(numPages)) as number[])
           .map((_, i: number) => i + 1)
           .map((page: number) => {
@@ -30,7 +38,7 @@ export default function PdfViewer({ documentPath }: { documentPath: string }) {
                   pageNumber={page}
                   renderAnnotationLayer={false}
                   renderTextLayer={false}
-                  scale={1.4}
+                  scale={1.49}
                 />
                 {page === 1 && <Separator className="h-2" />}
               </div>
