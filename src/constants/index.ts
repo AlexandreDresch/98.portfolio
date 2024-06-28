a8ed3055bf0a42df2c588552287239803751cad6
AlexandreDresch <connectWithAlexandre@proton.me>
2024-06-27 23:59:29 -0300

    feat: add FooterMessages to constants

---
+++ src/constants/index.ts
@@ -0,0 +1,237 @@
+export const FooterMessages = [
+  {
+    name: "New",
+    message: "Creates a new Folder, Shortcut, Document, etc...",
+  },
+  {
+    name: "Folder",
+    message: "Creates a new folder.",
+  },
+  {
+    name: "Shortcut",
+    message: "Creates a shortcut to quickly access your files or applications.",
+  },
+  {
+    name: "TextDocument",
+    message: "Creates a new text document for your notes and simple writings.",
+  },
+  {
+    name: "WordPadDocument",
+    message: "Creates a new WordPad document for rich text editing.",
+  },
+  {
+    name: "BitmapImage",
+    message: "Creates a new bitmap image for your drawings and graphics.",
+  },
+  {
+    name: "WaveSound",
+    message: "Creates a new wave sound file for your audio recordings.",
+  },
+  {
+    name: "CreateShortcut",
+    message: "Creates shortcuts to the selected items.",
+  },
+  {
+    name: "Delete",
+    message: "Deletes the selected item.",
+  },
+  {
+    name: "Rename",
+    message: "Renames the selected item.",
+  },
+  {
+    name: "Properties",
+    message: "Displays the properties of the selected items.",
+  },
+  {
+    name: "WorkOffline",
+    message: "Shows Web pages without downloading them.",
+  },
+  {
+    name: "Close",
+    message: "Closes the current window.",
+  },
+  {
+    name: "Undo",
+    message: "Undo the last action.",
+  },
+  {
+    name: "CutCtrl+X",
+    message: "Removes the selected items and copies them onto the Clipboard.",
+  },
+  {
+    name: "CopyCtrl+C",
+    message:
+      "Copies the selected items to the Clipboard. To put them in the new location, use the Paste command.",
+  },
+  {
+    name: "PasteCtrl+V",
+    message:
+      "Inserts the items you have copied or cut into the selected location.",
+  },
+  {
+    name: "PasteShortcut",
+    message:
+      "Creates shortcuts to the items you have copied or cut into the selected location.",
+  },
+  {
+    name: "SelectAllCtrl+A",
+    message: "Select all items in window.",
+  },
+  {
+    name: "InvertSelection",
+    message: "Reverses which items are selected and which are not.",
+  },
+  {
+    name: "Toolbars",
+    message: "Shows or hides toolbars",
+  },
+  {
+    name: "StandardButtons",
+    message: "Displays the Standard Buttons toolbar.",
+  },
+  {
+    name: "AddressBar",
+    message: "Displays the Address bar.",
+  },
+  {
+    name: "Links",
+    message: "Displays the Quick Links bar.",
+  },
+  {
+    name: "Radio",
+    message: "Displays the Radio bar.",
+  },
+  {
+    name: "TextLabels",
+    message: "Adds a text label under each toolbar button.",
+  },
+  {
+    name: "StatusBar",
+    message: "Shows or hides the status bar.",
+  },
+  {
+    name: "ExplorerBar",
+    message: "Search, Favorites and more...",
+  },
+  {
+    name: "SearchCtrl+E",
+    message: "Shows the Search bar.",
+  },
+  {
+    name: "FavoritesCtrl+I",
+    message: "Shows the Favorites bar.",
+  },
+  {
+    name: "HistoryCtrl+H",
+    message: "Shows the History bar.",
+  },
+  {
+    name: "Folders",
+    message: "Shows the Folders bar.",
+  },
+  {
+    name: "TipoftheDay",
+    message: "Shows a tip about the system.",
+  },
+  {
+    name: "asWebPage",
+    message: "Displays items in Web View.",
+  },
+  {
+    name: "LargeIcons",
+    message: "Displays items by using large icons.",
+  },
+  {
+    name: "SmallIcons",
+    message: "Displays items by using small icons.",
+  },
+  {
+    name: "List",
+    message: "Displays items in a list.",
+  },
+  {
+    name: "Details",
+    message: "Displays information about each item in the window.",
+  },
+  {
+    name: "ArrangeIcons",
+    message: "Contains commands for arranging items in the window.",
+  },
+  {
+    name: "byName",
+    message: "Sorts items alphabetically by name.",
+  },
+  {
+    name: "byType",
+    message: "Sorts items by type.",
+  },
+  {
+    name: "bySize",
+    message: "Sorts items by size, from smallest to largest.",
+  },
+  {
+    name: "byDate",
+    message: "Sorts items by date, from oldest to most recent.",
+  },
+  {
+    name: "AutoArrange",
+    message: "Arranges the icons automatically.",
+  },
+  {
+    name: "LineUpIcons",
+    message: "Arranges icons in a grid.",
+  },
+  {
+    name: "RefreshF5",
+    message: "Refreshes the content of current page.",
+  },
+  {
+    name: "FolderOptions...",
+    message: "Enables you to change settings.",
+  },
+  {
+    name: "BackAlt+LeftArrow",
+    message: "Goes to the previous page.",
+  },
+  {
+    name: "ForwardAlt+RightArrow",
+    message: "Goes to the next page.",
+  },
+  {
+    name: "UpOneLevel",
+    message: "Goes up one level.",
+  },
+  {
+    name: "HomePageAlt+Home",
+    message: "Goes to your home page.",
+  },
+  {
+    name: "ChannelGuide",
+    message: "Opens the Channel Guide page.",
+  },
+  {
+    name: "SearchtheWeb",
+    message: "Opens Internet Explorer.",
+  },
+  {
+    name: "MyComputer",
+    message: "Opens My Computer.",
+  },
+  {
+    name: "InternetCall",
+    message: "Opens your Internet call and meeting program.",
+  },
+  {
+    name: "AddtoFavorites...",
+    message: "Adds the current page to your Favorites list.",
+  },
+  {
+    name: "OrganizeFavorites...",
+    message: "Opens the Favorites folder.",
+  },
+  {
+    name: "(empty)",
+    message: "I'm just an empty space.",
+  },
+];
