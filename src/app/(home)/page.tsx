51caf0645cd691a15c94daa37c4345b97f40ad92
AlexandreDresch <connectWithAlexandre@proton.me>
2024-07-26 23:51:46 -0300

    refactor: moved ReduxProvider from Home to layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,11 @@
-"use client";
-
 import Desktop from "@/components/desktop/desktop";
 import Dock from "@/components/dock/dock";
-import ReduxProvider from "@/store/redux-provider";
 
 export default function Home() {
   return (
-    <ReduxProvider>
-      <main className="flex min-h-screen flex-col bg-[url('/windows-98-cloud.jpg')] box-border">
-        <Desktop />
-        <Dock />
-      </main>
-    </ReduxProvider>
+    <main className="flex min-h-screen flex-col bg-[url('/windows-98-cloud.jpg')] box-border crt">
+      <Desktop />
+      <Dock />
+    </main>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Titillium_Web } from "next/font/google";
 import "./globals.css";
 
+import ReduxProvider from "@/store/redux-provider";
+
 const titillium = Titillium_Web({
   subsets: ["latin"],
   weight: ["200", "300", "400", "600", "700", "900"],
@@ -9,7 +11,7 @@ const titillium = Titillium_Web({
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio",
+  title: "98.Portfolio",
   description: "Alexandre Dresch's portfolio",
 };
 
@@ -19,8 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={titillium.className}>{children}</body>
-    </html>
+    <ReduxProvider>
+      <html lang="en">
+        <body className={titillium.className}>{children}</body>
+      </html>
+    </ReduxProvider>
   );
 }
