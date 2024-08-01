d233c3327c6db19d9946f0a39d7f2a56610edc74
AlexandreDresch <connectWithAlexandre@proton.me>
2024-08-01 00:18:05 -0300

    feat: add dynamic folders to Desktop component

--- src/components/desktop/desktop.tsx
+++ src/components/desktop/desktop.tsx
@@ -2,65 +2,54 @@
 
 import Clippy from "../shared/clippy";
 import Shortcut from "../dock/shortcut";
-import { useAppDispatch, useAppSelector } from "@/store/store";
+import { useAppSelector } from "@/store/store";
 import ProjectContainer from "../folders/project-container";
 
 export default function Desktop() {
-  const dispatch = useAppDispatch();
   const { frontend, backend, mobile } = useAppSelector(
     (state) => state.projects
   );
+  const folders = useAppSelector((state) => state.folders.folders);
 
   return (
     <div className="pt-4 flex flex-col gap-8 min-w-[100dvw] min-h-[100dvh] relative overflow-hidden">
-      <Shortcut image="/recycle-bin.png" name="Recycle Bin" isDocument={false}>
-        <div className="flex h-[400px] justify-center items-center">
-          <p>Not available yet.</p>
-        </div>
-      </Shortcut>
-      <Shortcut
-        image="/computer-explorer.png"
-        name="My Computer"
-        isDocument={false}
-      >
-        <div className="flex h-[400px] justify-center items-center">
-          <p>Not available yet.</p>
-        </div>
-      </Shortcut>
-
-      <Shortcut image="/folder.png" name="BackEnd" isDocument={false}>
-        <ProjectContainer projects={backend} />
-      </Shortcut>
-
-      <Shortcut image="/folder.png" name="FrontEnd" isDocument={false}>
-        <ProjectContainer projects={frontend} />
-      </Shortcut>
-
-      <Shortcut image="/folder.png" name="Mobile" isDocument={false}>
-        <div className="flex h-[400px] justify-center items-center">
-          <p>Not available yet.</p>
-        </div>
-      </Shortcut>
-
-      <Shortcut
-        image="/file.png"
-        name="My Resume"
-        isDocument
-        documentPath="./englishCV.pdf"
-      />
-
-      <Shortcut
-        image="/help-book.png"
-        name="About"
-        isDocument
-        documentPath=""
-      />
-
-      <Shortcut image="/modem.png" name="Contact me" isDocument={false}>
-        <div className="flex h-[400px] justify-center items-center">
-          <p>Not available yet.</p>
-        </div>
-      </Shortcut>
+      {folders.map((folder) => (
+        <Shortcut
+          id={folder.id}
+          key={folder.id}
+          image={folder.image}
+          name={folder.name}
+          isOpen={folder.isOpen}
+          isDocument={folder.isDocument}
+          documentPath={folder.documentPath}
+          documentType={folder.documentType}
+        >
+          {folder.name === "Backend" && <ProjectContainer projects={backend} />}
+          {folder.name === "Frontend" && (
+            <ProjectContainer projects={frontend} />
+          )}
+          {folder.name === "Recycle Bin" && (
+            <div className="flex h-[400px] justify-center items-center">
+              <p>Not available yet.</p>
+            </div>
+          )}
+          {folder.name === "My Computer" && (
+            <div className="flex h-[400px] justify-center items-center">
+              <p>Not available yet.</p>
+            </div>
+          )}
+          {folder.name === "Mobile" && (
+            <div className="flex h-[400px] justify-center items-center">
+              <p>Not available yet.</p>
+            </div>
+          )}
+          {folder.name === "Contact me" && (
+            <div className="flex h-[400px] justify-center items-center">
+              <p>Not available yet.</p>
+            </div>
+          )}
+        </Shortcut>
+      ))}
 
       <Clippy />
     </div>
