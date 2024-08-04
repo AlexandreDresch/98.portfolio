cab4cb0cd47ad854f114f7efcd80623e58a98435
AlexandreDresch <connectWithAlexandre@proton.me>
2024-08-03 23:59:07 -0300

    refactor: add ternary operator to show project & add ImageSlider component

--- src/components/folders/project-container.tsx
+++ src/components/folders/project-container.tsx
@@ -6,6 +6,7 @@ import { useAppDispatch, useAppSelector } from "@/store/store";
 import { Project, ProjectContainerProps } from "@/types";
 import Image from "next/image";
 import { useState } from "react";
+import ImageSlider from "../shared/image-slider";
 
 export default function ProjectContainer({ projects }: ProjectContainerProps) {
   const dispatch = useAppDispatch();
@@ -24,50 +25,53 @@ export default function ProjectContainer({ projects }: ProjectContainerProps) {
     setOpenedProject(false);
   };
 
-  if (openedProject) {
-    return (
-      <div>
-        {/* Your logic to display the opened project goes here */}
-        <button onClick={handleCloseProject}>Close Project</button>
-      </div>
-    );
-  }
-
   return (
-    <div className="grid grid-cols-4 grid-rows-4 gap-4 gap-y-0 p-4 w-full h-[400px]">
-      {projects.map((project) => (
-        <div
-          key={project.id}
-          className="flex flex-col items-center cursor-pointer"
-          onClick={() => handleSelectProject(project)}
-          onDoubleClick={handleOpenProject}
-        >
-          <Image
-            src={
-              project.type === "FRONTEND"
-                ? "/icons/frontend-icon.png"
-                : project.type === "BACKEND"
-                ? "/icons/backend-icon.png"
-                : "/icons/mobile-icon.png"
-            }
-            alt={project.name}
-            width={40}
-            height={40}
-            className={cn(
-              selectedProject?.id === project.id && "brightness-75"
-            )}
-          />
-
-          <p
-            className={cn(
-              "text-sm",
-              selectedProject?.id === project.id && "text-white bg-[#010f80]"
-            )}
-          >
-            {project.name}
-          </p>
+    <div className="w-full h-[400px]">
+      {openedProject && selectedProject ? (
+        <div>
+          {selectedProject.images && (
+            <ImageSlider images={selectedProject.images} />
+          )}
+          <button onClick={handleCloseProject}>Close Project</button>
         </div>
-      ))}
+      ) : (
+        <div className={`grid grid-cols-4 grid-rows-4 gap-4 gap-y-0 p-4 ${openedProject && "hidden"}`}>
+          {projects.map((project) => (
+            <div
+              key={project.id}
+              className={`flex flex-col items-center cursor-pointer ${openedProject && "hidden"}`}
+              onClick={() => handleSelectProject(project)}
+              onDoubleClick={handleOpenProject}
+            >
+              <Image
+                src={
+                  project.type === "FRONTEND"
+                    ? "/icons/frontend-icon.png"
+                    : project.type === "BACKEND"
+                    ? "/icons/backend-icon.png"
+                    : "/icons/mobile-icon.png"
+                }
+                alt={project.name}
+                width={40}
+                height={40}
+                className={cn(
+                  selectedProject?.id === project.id && "brightness-75"
+                )}
+              />
+
+              <p
+                className={cn(
+                  "text-sm",
+                  selectedProject?.id === project.id &&
+                    "text-white bg-[#010f80]"
+                )}
+              >
+                {project.name}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
