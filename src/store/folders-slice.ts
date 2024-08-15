0bd0589c9f86978b800e59c5aa60fa4faaeacc2f
AlexandreDresch <connectWithAlexandre@proton.me>
2024-08-14 23:52:55 -0300

    feat: add openFolder, closeFolder & minimizeFolder to folders slice

--- src/store/folders-slice.ts
+++ src/store/folders-slice.ts
@@ -4,26 +4,58 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface FoldersState {
   folders: Folder[];
+  dockFolders: Folder[];
 }
 
 const initialState: FoldersState = {
   folders: folders,
+  dockFolders: [],
 };
 
 const foldersSlice = createSlice({
   name: "folders",
   initialState,
   reducers: {
-    toggleFolder(state, action: PayloadAction<number>) {
-      const folder = state.folders.find(
-        (folder) => folder.id === action.payload
-      );
+    openFolder(state, action: PayloadAction<number>) {
+      const folderId = action.payload;
+      const folder = state.folders.find((folder) => folder.id === folderId);
+
       if (folder) {
-        folder.isOpen = !folder.isOpen;
+        folder.isOpen = true;
+
+        if (!state.dockFolders.some((f) => f.id === folderId)) {
+          state.dockFolders.push(folder);
+        }
+      }
+    },
+
+    closeFolder(state, action: PayloadAction<number>) {
+      const folderId = action.payload;
+      const folder = state.folders.find((folder) => folder.id === folderId);
+
+      if (folder) {
+        folder.isOpen = false;
+        state.dockFolders = state.dockFolders.filter((f) => f.id !== folderId);
+      }
+    },
+
+    minimizeFolder(state, action: PayloadAction<number>) {
+      const folderId = action.payload;
+      const folder = state.folders.find((folder) => folder.id === folderId);
+      const dockFolder = state.dockFolders.find(
+        (folder) => folder.id === folderId
+      );
+
+      if (folder) {
+        folder.isOpen = false;
+      }
+
+      if (dockFolder) {
+        dockFolder.isOpen = false;
       }
     },
   },
 });
 
-export const { toggleFolder } = foldersSlice.actions;
+export const { openFolder, closeFolder, minimizeFolder } = foldersSlice.actions;
 export const foldersReducer = foldersSlice.reducer;
