a16c00376aa9c4b702d46be590bd14c12e0222e4
AlexandreDresch <connectWithAlexandre@proton.me>
2026-08-29 21:47:05 -0300

    fix: remove incorrect DocumentViewer import from FolderLayout component

--- src/components/folder/folder-layout.tsx
+++ src/components/folder/folder-layout.tsx
@@ -1,6 +1,5 @@
 import { FolderLayoutProps } from "@/types";
 import FolderHeader from "./folder-header";
-import DocumentViewer from "./document-viewer";
 import FolderContent from "./folder-content";
 
 export default function FolderLayout({ folder, children }: FolderLayoutProps) {
